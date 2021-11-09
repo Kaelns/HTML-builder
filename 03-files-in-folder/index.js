5575569d18a92c12909f5658041725cd144e1ce7
Yury Ahanouski <uraaru@live.com>
2021-11-09 21:55:49 +0300

    feat: complite files in folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+
+let pathToFolder = "./secret-folder";
+let newPathToFolder = pathToFolder;
+let pathToFile;
+let names;
+
+const showInfo = (basename, extension, size) => {
+  console.log(`${basename} - ${extension.slice(1)} - ${size}`);
+};
+
+const getSizeOfFile = async (pathToFile, basename, extension) => {
+  return fs.stat(pathToFile, (err, stats) => {
+    showInfo(basename, extension, stats.size);
+  });
+};
+
+const getInfoOfFiles = (names, pathTo) => {
+  names.forEach((name) => {
+    if (name.isFile()) {
+      const nameOfFile = name.name;
+      const extension = path.extname(nameOfFile)
+        ? path.extname(nameOfFile)
+        : " none";
+      const basename = path.basename(nameOfFile, `${extension}`);
+
+      pathToFile = path.join(__dirname, pathTo, nameOfFile);
+      getSizeOfFile(pathToFile, basename, extension);
+    } else {
+      newPathToFolder += `/${name.name}`;
+      readDir(newPathToFolder);
+    }
+  });
+};
+
+const readDir = async (pathTo) => {
+  try {
+    names = await fs.promises.readdir(path.join(__dirname, pathTo), {
+      withFileTypes: true,
+    });
+
+    getInfoOfFiles(names, pathTo);
+  } catch (e) {
+    console.log("e", e);
+  }
+};
+
+readDir(pathToFolder);
