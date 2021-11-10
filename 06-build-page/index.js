36bfcf25878ae6dd0eb09de64f95f9ee3b2913a5
Yury Ahanouski <uraaru@live.com>
2021-11-10 22:04:44 +0300

    feat: 06 add styles merging

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,10 @@
-const { join } = require("path");
+const { join, extname } = require("path");
 const { readdir, copyFile, mkdir, rmdir } = require("fs/promises");
+const { createWriteStream, createReadStream } = require("fs");
 
 const dist = join(__dirname, "project-dist");
+const fileStyleBundle = join(__dirname, "project-dist", "style.css");
+
 const createDir = async (folder) => {
   await rmdir(folder, { recursive: true });
   await mkdir(folder, { recursive: true });
@@ -28,10 +31,38 @@ const copyFiles = async (dirname, folder, dirnameCopy, folderCopy) => {
   console.log("Copy is complete");
 };
 
-// copyFiles(__dirname, "files", __dirname, "files-copy");
+const findCSSFiles = async (dirname, folder, writeStreamCSS) => {
+  const pathToFolder = join(dirname, folder);
+  const files = await readdir(pathToFolder, { withFileTypes: true });
+
+  files.forEach(async (file) => {
+    if (file.isFile()) {
+      const extension = extname(file.name);
+      const pathToFile = join(pathToFolder, file.name);
+
+      if (extension === ".css") {
+        const readStream = createReadStream(pathToFile, "utf-8");
+
+        readStream.pipe(writeStreamCSS);
+      }
+    } else {
+      findCSSFiles(pathToFolder, file.name);
+    }
+  });
+
+  console.log("Merge is complete");
+};
+
+const mergeStyles = async () => {
+  const writeStreamCSS = createWriteStream(fileStyleBundle, "utf-8");
+
+  findCSSFiles(__dirname, "styles", writeStreamCSS);
+};
+
 const buildingProject = async () => {
   await createDir(dist);
   await copyFiles(__dirname, "assets", dist, "assets");
+  await mergeStyles();
 };
 
 buildingProject();
