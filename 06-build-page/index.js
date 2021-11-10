78246919c64f39bdc3af73a08f6d699b249b0989
Yury Ahanouski <uraaru@live.com>
2021-11-10 21:09:42 +0300

    feat: 06 add copy of assets part

---
+++ 06-build-page/index.js
@@ -0,0 +1,37 @@
+const { join } = require("path");
+const { readdir, copyFile, mkdir, rmdir } = require("fs/promises");
+
+const dist = join(__dirname, "project-dist");
+const createDir = async (folder) => {
+  await rmdir(folder, { recursive: true });
+  await mkdir(folder, { recursive: true });
+};
+
+const copyFiles = async (dirname, folder, dirnameCopy, folderCopy) => {
+  const pathToFolder = join(dirname, folder);
+  const pathToFolderCopy = join(dirnameCopy, folderCopy);
+
+  await createDir(pathToFolderCopy);
+  const files = await readdir(pathToFolder, { withFileTypes: true });
+
+  files.forEach(async (file) => {
+    if (file.isFile()) {
+      const pathToFile = join(pathToFolder, file.name);
+      const pathToFileCopy = join(pathToFolderCopy, file.name);
+
+      await copyFile(pathToFile, pathToFileCopy);
+    } else {
+      copyFiles(pathToFolder, file.name, pathToFolderCopy, file.name);
+    }
+  });
+
+  console.log("Copy is complete");
+};
+
+// copyFiles(__dirname, "files", __dirname, "files-copy");
+const buildingProject = async () => {
+  await createDir(dist);
+  await copyFiles(__dirname, "assets", dist, "assets");
+};
+
+buildingProject();
