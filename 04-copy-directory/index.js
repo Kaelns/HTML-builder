5e179dba31f53b6ef41694b0fbf745105ab56e68
Yury Ahanouski <uraaru@live.com>
2021-11-10 15:21:09 +0300

    feat: complite copu to dir task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const { join } = require("path");
+const { readdir, copyFile, mkdir } = require("fs/promises");
 
+const createDir = async (folder) => {
+  mkdir(folder, { recursive: true });
+};
+
+const copyFiles = async (dirname, folder, dirnameCopy, folderCopy) => {
+  const pathToFolder = join(dirname, folder);
+  const pathToFolderCopy = join(dirnameCopy, folderCopy);
+
+  await createDir(pathToFolderCopy);
+
+  const files = await readdir(pathToFolder, { withFileTypes: true });
+
+  files.forEach(async (file) => {
+    if (file.isFile()) {
+      const pathToFile = join(pathToFolder, file.name);
+      const pathToFileCopy = join(pathToFolderCopy, file.name);
+
+      await copyFile(pathToFile, pathToFileCopy);
+    } else {
+      copyFiles(pathToFolder, file.name, pathToFolderCopy, file.name);
+    }
+  });
+
+  console.log("Copy is complete");
+};
+
+copyFiles(__dirname, "files", __dirname, "files-copy");
